8b81b05a2a64c4324dc8ddc73f5e5ff0a2abd757
Dhruv Aggarwal <104587793+aggarwaldhruv1@users.noreply.github.com>
2022-05-27 23:05:19 +0530

    Update Assignment4 Que3.py

--- Assignment4 Que3.py
+++ Assignment4 Que3.py
@@ -8,6 +8,10 @@ import random
 
 print('Assignment Question 3')
 
+#We will import the "random" module to generate random integers.
+
+print("Multiplication game program for kids.")
+
 for i in range(1, 11):
     a = random.randint(0, 9)
     b = random.randint(0, 9)
@@ -16,4 +20,3 @@ for i in range(1, 11):
         print('Right!')
     else:
         print('Wrong. The correct answer is', a * b)
-
